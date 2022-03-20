986d262e6354496bbb5b2be5d7908e35a82c5d4d
Numan Ahmed <mdnmnahmed@gmail.com>
2022-03-20 11:05:17 +0530

    Project Section design

---
+++ src/Components/Project.jsx/Project.jsx
@@ -0,0 +1,29 @@
+export const Project = () => {
+    return (
+        <section class="text-gray-600 body-font">
+            <div class="container px-5 py-24 mx-auto">
+                <div>
+                    <div class="bg-gray-300 p-5 rounded-lg">
+                        NNN
+                        <p>Some data</p>
+                        <p>Some data Another Data</p>
+                        <p>I'm Numan, a Full Stack Developer</p>
+                    </div>
+
+                    <div class="bg-gray-400 p-5 rounded-lg w-28 relative -top-10 left-3">NNN</div>
+                </div>
+
+                <div class="">
+                    <div class="bg-gray-300 p-5 rounded-lg">
+                        NNN
+                        <p>Some data</p>
+                        <p>Some data Another Data</p>
+                        <p>I'm Numan, a Full Stack Developer</p>
+                    </div>
+
+                    <div class="bg-gray-400 p-5 rounded-lg w-28 relative -top-10 -left-3 ml-auto">NNN</div>
+                </div>
+            </div>
+        </section>
+    )
+}
